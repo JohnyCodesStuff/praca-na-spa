f0cb31ee7c759c17036426354633829ebf5f0d66
JohnyCodesStuff <janpelczar12@gmail.com>
2025-01-04 23:38:53 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './rose_logo.svg';
 import './App.css';
+import react, {useState} from 'react';
 
-function App() {
+
+
+const App = () => {
+
+  const [text,setText] = useState("Yes")
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,12 +16,12 @@ function App() {
         <p>
           Will You be my Valentine?
         </p>
-        <button>
-          Yes
+        <button className="button" onClick={() => setText("Woohoooooooo!!!!!!!")}>
+          {text}
         </button>
-        <button>
+        <button className="button">
           No
-        </button>
+        </button >
       </header>
     </div>
   );
